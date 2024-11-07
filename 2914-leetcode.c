c4e2d548366fb0985ed4d258847cd31c6c760c32
hwangs12 <sungjun94@gmail.com>
2024-11-06 22:47:13 -0500

    commit

---
+++ 2914-leetcode.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int minChanges(char* s)
+{
+    int count = 0;
+    for (int i = 0; i + 1 < strlen(s); i+=2)
+    {
+        if (s[i] != s[i+1])
+        {
+            count++;
+        }
+    }
+
+    return count;
+}
+
+int main()
+{
+    char * coco = "10001011";
+    int nana = minChanges(coco);
+    printf("%d", nana); // answer should be 2. 
+    return 0;
+}
---
+++ README.md
@@ -0,0 +1,9 @@
+## Just a repo for problem solving
+
+## All through C language
+
+## Solution will be someone else's
+
+## this is just exposition of how I'm understanding those solutions. 
+
+## i'm not that smart. 
