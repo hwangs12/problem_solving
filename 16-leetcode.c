84903757545f963ea445486c8016d86211146d76
hwangs12 <sungjun94@gmail.com>
2024-11-17 00:26:56 -0500

    hihi

--- 16-leetcode.c
+++ 16-leetcode.c
@@ -7,5 +7,28 @@ int compare(const void *a, const void *b) {
 }
 
 int threeSumClosest(int* nums, int numsSize, int target) {
+    int minDiff;
     
+    qsort(nums, numsSize, sizeof(int), compare);
+
+    int r = numsSize - 1;
+    int l = 1;
+
+    for (int i = 0; i < numsSize; i++) {
+        while (l < r) {
+            int sum = nums[i] + nums[r] + nums[l];
+
+            if (sum < target) {
+                l++;
+            } else if (sum > target) {
+                r--;
+            }
+        }
+        
+    }
+}
+
+int main()
+{
+
 }
