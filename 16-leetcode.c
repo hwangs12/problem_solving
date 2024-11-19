bae1cbe279731aaf2efcd09149517ab2c6cdbfaa
hwangs12 <sungjun94@gmail.com>
2024-11-19 00:59:42 -0500

    think diff

--- 16-leetcode.c
+++ 16-leetcode.c
@@ -10,16 +10,22 @@ int compare(const void *a, const void *b) {
 }
 
 int threeSumClosest(int* nums, int numsSize, int target) {
-    
     qsort(nums, numsSize, sizeof(int), compare);
 
     int r = numsSize - 1;
     int l = 1;
+    // int minDiff = nums[r] - nums[l];
     int sum;
+    // int difference;
     for (int i = 0; i < numsSize; i++) {
+        r = numsSize - 1;
+        l = i + 1;
         while (l < r) {
             sum = nums[i] + nums[r] + nums[l];
-
+            // difference = abs(sum - target);
+            // if (difference < minDiff) {
+            //     minDiff = difference;
+            // }
             if (sum < target) {
                 l++;
             } else if (sum > target) {
@@ -35,7 +41,7 @@ int threeSumClosest(int* nums, int numsSize, int target) {
 
 int main()
 {
-    int nums[4] = {-1, 2, 1, -4};
-    int co = threeSumClosest(nums, 4, 1);
+    int nums[4] = {1,1,1,0};
+    int co = threeSumClosest(nums, 4, -100);
     printf("The closest sum is %d.\n", co);
 }
